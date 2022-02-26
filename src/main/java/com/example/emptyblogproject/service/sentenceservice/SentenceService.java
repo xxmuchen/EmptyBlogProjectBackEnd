14b84aa4d8e43ef3711e57094e0b15dcb343fc75
xxmuchen <2879257224@qq.com>
2022-02-26 13:03:26 +0800

    变更

---
+++ src/main/java/com/example/emptyblogproject/service/sentenceservice/SentenceService.java
@@ -0,0 +1,18 @@
+package com.example.emptyblogproject.service.sentenceservice;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.emptyblogproject.bean.sentence.Sentence;
+import com.example.emptyblogproject.mapper.sentencemapper.SentenceMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 王程翔
+ * Date: 2022/2/25
+ * Time: 0:36
+ * Description:
+ */
+
+public interface SentenceService extends IService<Sentence> {
+}
